107420ed24415cc88130b07661da8e9993ecbe18
dceoy <dnarsil+github@gmail.com>
2024-01-14 23:24:30 +0900

    Add envs/dev/main.tf

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# terraform-aws-ecs-with-ssm
+terraform-aws-ecs-with-ssm
+==========================
+
 Terraform module of ECS with SSM
+
+[![Lint](https://github.com/dceoy/terraform-aws-ecs-with-ssm/actions/workflows/lint.yml/badge.svg)](https://github.com/dceoy/terraform-aws-ecs-with-ssm/actions/workflows/lint.yml)
+
+Installation
+------------
+
+1.  Check out the repository.
+
+    ```sh
+    $ git clone https://github.com/dceoy/terraform-aws-ecs-with-ssm.git
+    $ cd terraform-aws-ecs-with-ssm
+    ````
+
+2.  Install [AWS CLI](https://aws.amazon.com/cli/) and set `~/.aws/config` and `~/.aws/credentials`.
+
+3.  Create a S3 bucket and a DynamoDB table for Terraform.
+    (cf. [terraform-aws-vpc-for-slc](https://github.com/dceoy/terraform-aws-vpc-for-slc))
+
+4.  Create configuration files.
+
+    ```sh
+    $ cp env/dev/example.tfbackend env/dev/aws.tfbackend
+    $ cp env/dev/example.tfvars env/dev/dev.tfvars
+    $ vi env/dev/aws.tfbackend
+    $ vi env/dev/dev.tfvars
+    ```
+
+5.  Initialize a new Terraform working directory.
+
+    ```sh
+    $ terraform -chdir='envs/dev/' init -reconfigure -backend-config='./aws.tfbackend'
+    ```
+
+6.  Generates a speculative execution plan. (Optional)
+
+    ```sh
+    $ terraform -chdir='envs/dev/' plan -var-file='./dev.tfvars'
+    ```
+
+7.  Creates or updates infrastructure.
+
+    ```sh
+    $ terraform -chdir='envs/dev/' apply -var-file='./dev.tfvars' -auto-approve
+    ```
+
+Cleanup
+-------
+
+```sh
+$ terraform -chdir='envs/dev/' apply -var-file='./dev.tfvars' -auto-approve -destroy
+```
---
+++ envs/dev/main.tf
@@ -0,0 +1,41 @@
+module "vpc" {
+  source              = "github.com/dceoy/terraform-aws-vpc-for-slc//modules/vpc?ref=v0.1.0"
+  system_name         = var.system_name
+  env_type            = var.env_type
+  vpc_cidr_block      = var.vpc_cidr_block
+  enable_vpc_flow_log = var.enable_vpc_flow_log
+}
+
+module "subnet" {
+  source               = "github.com/dceoy/terraform-aws-vpc-for-slc//modules/subnet?ref=v0.1.0"
+  vpc_id               = module.vpc.vpc_id
+  system_name          = var.system_name
+  env_type             = var.env_type
+  private_subnet_count = var.private_subnet_count
+  public_subnet_count  = var.public_subnet_count
+  subnet_newbits       = var.subnet_newbits
+}
+
+module "nat" {
+  source                  = "github.com/dceoy/terraform-aws-vpc-for-slc//modules/nat?ref=v0.1.0"
+  count                   = var.create_nat_gateways && var.public_subnet_count > 0 && var.private_subnet_count > 0 ? 1 : 0
+  public_subnet_ids       = module.subnet.public_subnet_ids
+  private_route_table_ids = module.subnet.private_route_table_ids
+  system_name             = var.system_name
+  env_type                = var.env_type
+}
+
+module "vpce" {
+  source             = "github.com/dceoy/terraform-aws-vpc-for-slc//modules/vpce?ref=v0.1.0"
+  count              = var.create_vpc_interface_endpoints && var.private_subnet_count > 0 ? 1 : 0
+  private_subnet_ids = module.subnet.private_subnet_ids
+  security_group_ids = [module.subnet.private_security_group_id]
+  system_name        = var.system_name
+  env_type           = var.env_type
+}
+
+module "ssm" {
+  source      = "github.com/dceoy/terraform-aws-vpc-for-slc//modules/ssm?ref=v0.1.0"
+  system_name = var.system_name
+  env_type    = var.env_type
+}
