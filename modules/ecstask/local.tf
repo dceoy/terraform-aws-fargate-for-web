d1d44bff95aa008cf2175971d98f6a861779d9b1
dceoy <dceoy@users.noreply.github.com>
2025-02-13 00:21:47 +0900

    Add health check configurations

--- modules/ecstask/local.tf
+++ modules/ecstask/local.tf
@@ -2,5 +2,5 @@ data "aws_region" "current" {}
 
 locals {
   region                     = data.aws_region.current.name
-  ecs_task_definition_family = var.ecs_task_definition_family != null ? var.ecs_task_definition_family : "${var.system_name}-${var.env_type}-ecs-task-definition"
+  ecs_task_definition_family = var.ecs_task_definition_family != null ? var.ecs_task_definition_family : "${var.system_name}-${var.env_type}-ecs-task"
 }
